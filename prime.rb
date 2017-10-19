1921200478482b5a0f09753d9190f6e2f40948a6
Julianne Kim <juliannne@gmail.com>
2017-10-19 02:02:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 # Add  code here!
 def prime?(n)
-  if n <= 1 
-    return false 
-  end 
+  if n <= 1
+    return false
+  end
   x = 2
-  while x < n 
-    if n % x == 0 
-      return false 
-    end 
-    x += 1 
-  end 
+  while x < n
+    if n % x == 0
+      return false
+    end
+    x += 1
+  end
   return true
 end
